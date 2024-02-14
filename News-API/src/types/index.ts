09f1d1660038f1815b404e9e6e5454474b326680
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-02-14 11:41:37 +0300

    feat: add folder types

---
+++ News-API/src/types/index.ts
@@ -0,0 +1,14 @@
+export interface NewsApi {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+
+  source: {
+    id: string;
+    name: string;
+  };
+  title: string;
+  url: string;
+  urlToImage: string;
+}
