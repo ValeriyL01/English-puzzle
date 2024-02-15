123bc48ced16ec3d47db0147ce9e552279ce442f
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-02-15 11:44:16 +0300

    feat: migrated to ts file appView.js

--- News-API/src/components/view/appView.ts
+++ News-API/src/components/view/appView.ts
@@ -1,18 +1,14 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { DataNews, DataSource } from '../../types';
 
 export class AppView {
-  constructor() {
-    this.news = new News();
-    this.sources = new Sources();
-  }
-
-  drawNews(data) {
+  public static drawNews(data: DataNews): void {
     const values = data?.articles ? data?.articles : [];
     News.draw(values);
   }
 
-  drawSources(data) {
+  public static drawSources(data: DataSource): void {
     const values = data?.sources ? data?.sources : [];
     Sources.draw(values);
   }
--- News-API/src/types/index.ts
+++ News-API/src/types/index.ts
@@ -16,3 +16,49 @@ export interface SourcesAPI {
   id: string;
   name: string;
 }
+export interface DataNews {
+  status: string;
+  totalResults: number;
+  articles: [{
+    source:
+    {
+      id: string;
+      name: string;
+      description : string;
+      urlToImage: string;
+      category: string;
+      language: string;
+      country: string;
+      author: string;
+    },
+    author: string;
+    content: string;
+    description : string;
+    publishedAt: string;
+    status:string;
+    title: string;
+    url: string;
+    urlToImage: string;
+    id: string;
+    name: string;
+    language: string;
+    country: string;
+    category:string;
+    totalResults:number;
+  }]
+
+}
+export interface DataSource {
+  status: string;
+  sources: SourceItem[];
+}
+
+export interface SourceItem {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  category: string;
+  language: string;
+  country: string;
+}
