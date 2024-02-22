08619c9365d8346e5cc5c709d6e4ec4f71ea2ded
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-02-22 09:41:15 +0300

    refactor: changed URL

--- News-API/src/components/controller/appLoader.ts
+++ News-API/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://rss-news-api.onrender.com/', {
       apiKey: '1a5da4b8655a4d0e8b7a9d240ac3c390', // получите свой ключ https://newsapi.org/
     });
   }
