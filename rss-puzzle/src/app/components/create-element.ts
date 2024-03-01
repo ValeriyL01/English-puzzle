83941d4a48ca4d1298dd6a64951b3435a8c14dc0
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-03-01 20:14:32 +0300

    feat: RSS-PZ-01 Implement Login Form (#2)

--- rss-puzzle/src/app/components/create-element.ts
+++ rss-puzzle/src/app/components/create-element.ts
@@ -1,10 +1,23 @@
-function createElement(tag: string, className?: string, text?: string): HTMLElement {
+function createElement(
+  tag: string,
+  className?: string,
+  text?: string,
+  attributes?: { [key: string]: string },
+): HTMLElement {
   const element = document.createElement(tag);
+
   if (className) {
     element.className = className;
   }
+
   if (text) {
     element.textContent = text;
+  }
+
+  if (attributes) {
+    Object.keys(attributes).forEach((atr) => {
+      element.setAttribute(atr, attributes[atr]);
+    });
   }
 
   return element;
--- rss-puzzle/src/app/components/login-form/login-form.ts
+++ rss-puzzle/src/app/components/login-form/login-form.ts
@@ -7,9 +7,21 @@ loginFormModalWrapper.append(loginFormModal);
 const form = createElement('form', 'form');
 const titleForm = createElement('h2', 'form-title', 'Login');
 loginFormModal.append(titleForm, form);
-const firstNameInput = createElement('input', 'first-name-input');
-const surnameInput = createElement('input', 'surname-input');
-const submitButton = createElement('button', 'button', 'Login');
+const firstNameInput = createElement('input', 'first-name-input', '', {
+  type: 'text',
+  name: 'first-name',
+  placeholder: 'First Name',
+  required: 'true',
+});
+const surnameInput = createElement('input', 'surname-input', '', {
+  type: 'text',
+  name: 'surname-name',
+  placeholder: 'Surname',
+  required: 'true',
+});
+const submitButton = createElement('button', 'button', 'Login', {
+  type: 'submit',
+});
 form.append(
   createElement('p', '', 'First Name'),
   firstNameInput,
@@ -18,11 +30,4 @@ form.append(
   submitButton,
 );
 
-firstNameInput.setAttribute('type', 'text');
-firstNameInput.setAttribute('name', 'first-name');
-firstNameInput.setAttribute('placeholder', 'First Name');
-surnameInput.setAttribute('type', 'text');
-surnameInput.setAttribute('name', 'surname-name');
-surnameInput.setAttribute('placeholder', 'Surname');
-submitButton.setAttribute('type', 'submit');
 export default loginFormModalWrapper;
