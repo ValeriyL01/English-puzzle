1305722fe4471b97e8ab3deb7036ac39ef2eee22
Valeriy Lastovka <107634274+ValeriyL01@users.noreply.github.com>
2024-03-03 21:25:53 +0300

    feat: RSS-PZ-06_display personalized greeting(#1) (#34)

--- rss-puzzle/src/app/app.ts
+++ rss-puzzle/src/app/app.ts
@@ -2,7 +2,7 @@ import createElement from './components/createElement';
 import { loginFormModalWrapper } from './components/loginForm/loginForm';
 import { checkUserLocalStorage } from './components/localStorage';
 import logoutButton from './components/logoutButton/logoutButton';
-import startScreen from './components/startScreen/startScreen';
+import { startScreen } from './components/startScreen/startScreen';
 
 export default function renderApp(): void {
   const container = createElement('div', 'container');
