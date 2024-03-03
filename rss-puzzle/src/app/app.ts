b9ec58aedf28f20aa853f420f61eb7956c30937b
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-03-03 17:50:11 +0300

    feat: RSS-PZ-04_designStartScreen(#1)

--- rss-puzzle/src/app/app.ts
+++ rss-puzzle/src/app/app.ts
@@ -2,10 +2,11 @@ import createElement from './components/createElement';
 import { loginFormModalWrapper } from './components/loginForm/loginForm';
 import { checkUserLocalStorage } from './components/localStorage';
 import logoutButton from './components/logoutButton/logoutButton';
+import startScreen from './components/startScreen/startScreen';
 
 export default function renderApp(): void {
   const container = createElement('div', 'container');
   document.body.append(container);
-  container.append(loginFormModalWrapper, logoutButton);
+  container.append(loginFormModalWrapper, logoutButton, startScreen);
 }
 checkUserLocalStorage(loginFormModalWrapper);
