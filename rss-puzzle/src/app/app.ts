8b5121122dec80ad5747dae86db38fd050dcc216
Valeriy Lastovka <107634274+ValeriyL01@users.noreply.github.com>
2024-03-04 12:37:37 +0300

    feat/RSS-PZ-07_ImplimentStartBbutton (#35)
    * feat: RSS-PZ-07_Impliment start button(#1)
    
    * feat: RSS-PZ-07_Impliment start button(#2)

--- rss-puzzle/src/app/app.ts
+++ rss-puzzle/src/app/app.ts
@@ -2,11 +2,35 @@ import createElement from './components/createElement';
 import { loginFormModalWrapper } from './components/loginForm/loginForm';
 import { checkUserLocalStorage } from './components/localStorage';
 import logoutButton from './components/logoutButton/logoutButton';
-import { startScreen } from './components/startScreen/startScreen';
+import { startScreen, startScreenButton } from './components/startScreen/startScreen';
+import gamePages from './pages/gamePages';
 
+const container = createElement('div', 'container');
 export default function renderApp(): void {
-  const container = createElement('div', 'container');
   document.body.append(container);
   container.append(loginFormModalWrapper, logoutButton, startScreen);
 }
+
+function switchGamePages(page: HTMLElement): void {
+  [loginFormModalWrapper, startScreen].forEach((element) => {
+    container.removeChild(element);
+  });
+  container.append(logoutButton, page);
+}
+function switchLoginForm(): void {
+  if (container.contains(gamePages)) {
+    container.removeChild(gamePages);
+  }
+  [loginFormModalWrapper, startScreen].forEach((element) => {
+    container.append(element);
+  });
+  container.append(logoutButton);
+}
+startScreenButton.addEventListener('click', () => {
+  switchGamePages(gamePages);
+});
+logoutButton.addEventListener('click', () => {
+  switchLoginForm();
+});
+
 checkUserLocalStorage(loginFormModalWrapper);
---
+++ rss-puzzle/src/app/pages/gamePages.ts
@@ -0,0 +1,5 @@
+import createElement from '../components/createElement';
+
+const gamePages = createElement('div', 'game-pages');
+
+export default gamePages;
