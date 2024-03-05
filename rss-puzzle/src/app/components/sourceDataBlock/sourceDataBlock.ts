7e8f5f73b9255444cce3827ea5c38edc5b4ca871
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-03-05 10:32:49 +0300

    feat: RSS-PZ-09_dynamic sizing word cards(#1)

--- rss-puzzle/src/app/components/sourceDataBlock/sourceDataBlock.ts
+++ rss-puzzle/src/app/components/sourceDataBlock/sourceDataBlock.ts
@@ -11,12 +11,19 @@ const { textExample } = wordCollectionLevel1.rounds[0].words[0];
 // создание блоков со словами в рандомном порядке
 function createWordsBlock(sentence: string): void {
   const words = sentence.split(' ');
+  const widthParent = 768;
+  const wordLengthInit = words.length;
+  const averageWidth = widthParent / wordLengthInit;
 
   words
     .sort(() => Math.random() - 0.5)
-    .forEach((word: string) => {
-      const wordCard = createElement('div', 'word-card', `${word}`);
+    .forEach((word) => {
+      const wordCard = createElement('div', 'word-card', word);
       sourseData.append(wordCard);
+      wordCard.style.width = `${averageWidth}px`; // Фиксированная ширина каждого элемента
+      const wordWidth = word.length * 10;
+      const fontSize = wordWidth > widthParent / wordLengthInit ? '14px' : '18px';
+      wordCard.style.fontSize = fontSize;
       wordCard.addEventListener('click', () => {
         resultBlock.append(wordCard);
       });
