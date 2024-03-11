54e8c4566723a09b3219dc7a69d6233dc089aa71
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-03-11 16:35:09 +0300

    feat: RSS-PZ-20_style change audio icon(#1)

--- rss-puzzle/src/app/components/audioSentence/audioSentence.ts
+++ rss-puzzle/src/app/components/audioSentence/audioSentence.ts
@@ -4,12 +4,27 @@ import { currentLineDataWithPuzzles } from '../logicVictory';
 
 const audioSentence = createElement('div', 'audio-sentence');
 
+let isPlayAudio = true;
+
 function playAudio(audio: string): void {
   const soundText = new Audio(audio);
   soundText.volume = 0.5;
   soundText.play();
+  soundText.addEventListener('play', () => {
+    // Применяем keyframes анимацию к элементу audioSentence
+    audioSentence.style.animation = 'audioAnimation 0.5s infinite';
+  });
+  soundText.addEventListener('ended', () => {
+    audioSentence.style.animationPlayState = 'paused';
+    isPlayAudio = true;
+  });
 }
+
 audioSentence.addEventListener('click', () => {
-  playAudio(currentLineDataWithPuzzles.audio);
+  if (isPlayAudio) {
+    playAudio(currentLineDataWithPuzzles.audio);
+  }
+  isPlayAudio = false;
 });
+
 export { audioSentence, playAudio };
