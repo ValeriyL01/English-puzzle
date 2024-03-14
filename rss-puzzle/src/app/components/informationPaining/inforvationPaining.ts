593e68fe39792436867020dcc46b64b6cb8899ef
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-03-14 13:20:03 +0300

    feat: RSS-PZ-28_display information about completed paining(#1)

---
+++ rss-puzzle/src/app/components/informationPaining/inforvationPaining.ts
@@ -0,0 +1,5 @@
+import './informationPaining.css';
+import createElement from '../createElement';
+
+const informationPaining = createElement('div', 'information-paining', '');
+export default informationPaining;
