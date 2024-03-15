3a27740cfe2d7bb307c163416e23b987a5a6e195
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-03-15 10:17:27 +0300

    feat: implement distinct known and unknown sentence sections(#1)

--- rss-puzzle/src/app/pages/statisticsPages.ts
+++ rss-puzzle/src/app/pages/statisticsPages.ts
@@ -8,6 +8,59 @@ const continueButtonStatisticsPages = createElement(
   'continue-button-statistic',
   'Continue',
 ) as HTMLButtonElement;
+const autocompleteWrapper = createElement('div', 'autocomplete-wrapper');
+const knowsWrapper = createElement('div', 'knows-wrapper');
+const autocompleteTitle = createElement('h2', 'autocomplete-Title', 'Auto-Complete');
+const knowTitle = createElement('h2', 'know-Title', 'I know');
 statisticsPages.append(statisticsPagesContainer);
-statisticsPagesContainer.append(continueButtonStatisticsPages);
-export { statisticsPages, continueButtonStatisticsPages };
+statisticsPagesContainer.append(autocompleteWrapper, knowsWrapper, continueButtonStatisticsPages);
+autocompleteWrapper.append(autocompleteTitle);
+knowsWrapper.append(knowTitle);
+
+function createArrAutoCompleteSentence(arrAutoCompleteSentences: string[], entences: string): void {
+  arrAutoCompleteSentences.push(entences);
+}
+
+function addAutoCompleteSentences(
+  arrAutoCompleteSentences: string[],
+  arrAutoCompleteSentencesElements: HTMLElement[],
+): void {
+  arrAutoCompleteSentences.forEach((sentence: string) => {
+    const sentenceBlock = createElement('div', 'sentence');
+    arrAutoCompleteSentencesElements.push(sentenceBlock);
+    autocompleteWrapper.append(sentenceBlock);
+    sentenceBlock.innerText = sentence;
+  });
+  autocompleteTitle.innerText = `Auto-Complete (${arrAutoCompleteSentences.length})`;
+}
+function addKnowSentences(
+  arrAllSentences: string[],
+  arrAutoCompleteSentences: string[],
+  arrKnowSentences: string[],
+  arrKnowSentencesSentencesElements: HTMLElement[],
+): void {
+  arrAllSentences.forEach((sentence) => {
+    if (!arrAutoCompleteSentences.includes(sentence)) {
+      arrKnowSentences.push(sentence);
+    }
+  });
+  arrKnowSentences.forEach((sentence: string) => {
+    const sentenceBlock = createElement('div', 'sentence');
+    arrKnowSentencesSentencesElements.push(sentenceBlock);
+    knowsWrapper.append(sentenceBlock);
+    sentenceBlock.innerText = sentence;
+  });
+  knowTitle.innerText = `I know (${arrKnowSentences.length})`;
+}
+
+export {
+  statisticsPages,
+  continueButtonStatisticsPages,
+  autocompleteWrapper,
+  knowsWrapper,
+  autocompleteTitle,
+  knowTitle,
+  createArrAutoCompleteSentence,
+  addAutoCompleteSentences,
+  addKnowSentences,
+};
